b66e79b1fb93afc7d8a02562ed31a536aabb0a59
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-05-16 12:54:37 +0300

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Set a fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
